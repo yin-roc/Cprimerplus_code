b9a96d4acd5c3ecd62130c73d564037de4a19e66
yin-roc <yin15295770968@163.com>
2023-10-07 21:31:26 +0800

    14.3：例程14.7~14.9

---
+++ chapter14/exp/14.7-14.9/Project1/Worker0.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#ifndef __WORKER0_H__
+#define __WORKER0_H__
+
+#include <string>
+
+class Worker
+{
+private:
+	std::string fullname;
+	long id;
+public:
+	Worker() : fullname("no one"), id(0) {}
+	Worker(const std::string& s, long n) : fullname(s), id(n) {}
+	virtual ~Worker() {}
+	virtual void Set();
+	virtual void Show() const;
+};
+
+
+class Waiter : public Worker
+{
+private:
+	int panache;
+public:
+	Waiter() : Worker(), panache(0) {}
+	Waiter(const std::string& s, long n, int p = 0) : Worker(s, n), panache(p) {}
+	Waiter(const Worker& wk, int p = 0) : Worker(wk), panache(p) {}
+	void Set();
+	void Show() const;
+};
+
+
+class Singer : public Worker
+{
+protected:
+	enum {other, alto, contralto, soprano, bass, baritone, tenor};
+	enum {Vtypes = 7};
+private:
+	static const char* pv[Vtypes];
+	int voice;
+public:
+	Singer() : Worker(), voice(other) {}
+	Singer(const std::string& s, long n, int v = other) : Worker(s, n), voice(n) {}
+	Singer(const Worker& wk, int v = other) : Worker(wk), voice(v) {}
+	void Set();
+	void Show() const;
+};
+
+#endif
