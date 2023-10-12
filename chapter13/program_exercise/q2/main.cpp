36b2acef6dc195320d91b8935e44d96cf842433f
yin-roc <yin15295770968@163.com>
2023-10-12 12:17:19 +0800

    14.4例程14.15~14.16

--- chapter13/program_exercise/q2/main.cpp
+++ chapter13/program_exercise/q2/main.cpp
@@ -14,7 +14,9 @@ int main()
 
     Cd c1("Beatles", "Capitol", 14, 35.5);
     Classic c2 = Classic("Piano Sonata in B flat, Fantasia in C", "Alfred Brendel", "Philips", 2, 57.17);
-
+char ch;
+cout << "shuru:" << endl;
+cin >> ch;
     Cd * pcd = &c1;
 
     cout << "Using object directly:\n";
